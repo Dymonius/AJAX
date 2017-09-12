bc6c3fd12ac877359972a50eee9a6d985b2e7e5a
Dmitry <dymonius12@gmail.com>
2017-09-12 15:54:13 +0300

    lesson1

---
+++ content.php
@@ -0,0 +1,5 @@
+<?php
+    sleep(2);
+    echo "Data: name - ".$_POST['name'].", number - ".$_POST['number'];
+
+?>
---
+++ index.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Test</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+    <script>
+        function funcBefore() {
+            $("#information").text("wait...")
+        }
+
+        function funcSuccess(data) {
+            $("#information").text(data);
+        }
+
+        $(document).ready(function () {
+            $("#load").bind("click", function () {
+                var admin = "Admin";
+                $.ajax({
+                    url: "content.php",
+                    type: "POST",
+                    data: ({name: admin, number: 5}),
+                    dataType: "html",
+                    beforSend: funcBefore,
+                    success: funcSuccess
+                });
+            });
+        });
+    </script>
+</head>
+<body>
+<p id="loader" style="cursor: pointer">Load data</p>
+<div id="information"></div>
+</body>
+</html>
